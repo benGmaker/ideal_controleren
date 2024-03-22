02f5c89a8894850104e1dedb1e8cf595e876237e
Ben Gortemaker <b.f.gortemaker@student.tue.nl>
2024-03-22 12:38:21 +0100

    testdata voor onbekende klant

--- main.py
+++ main.py
@@ -3,51 +3,40 @@ import os
 import glob
 from pick import pick
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
 
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
 
-def _draw_as_table(df, pagesize):
-    alternating_colors = [['white'] * len(df.columns), ['lightgray'] * len(df.columns)] * len(df)
-    alternating_colors = alternating_colors[:len(df)]
-    fig, ax = plt.subplots(figsize=pagesize)
-    ax.axis('tight')
-    ax.axis('off')
-    the_table = ax.table(cellText=df.values,
-                         rowLabels=df.index,
-                         colLabels=df.columns,
-                         rowColours=['lightblue'] * len(df),
-                         colColours=['lightblue'] * len(df.columns),
-                         cellColours=alternating_colors,
-                         loc='center')
-    return fig
+table_style = TableStyle([
+    ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+    ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+    ('FONTSIZE', (0, 0), (-1, 0), 14),
+    ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+    ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+    ('TEXTCOLOR', (0, 1), (-1, -1), colors.black),
+    ('ALIGN', (0, 1), (-1, -1), 'CENTER'),
+    ('FONTNAME', (0, 1), (-1, -1), 'Helvetica'),
+    ('FONTSIZE', (0, 1), (-1, -1), 12),
+    ('BOTTOMPADDING', (0, 1), (-1, -1), 8),
+])
 
+def create_pdf(df,filename):
+    pdf = SimpleDocTemplate("dataframe.pdf", pagesize=letter)
 
-def dataframe_to_pdf(df, filename, numpages=(1, 1), pagesize=(11, 8.5)):
-    with PdfPages(filename) as pdf:
-        nh, nv = numpages
-        rows_per_page = len(df) // nh
-        cols_per_page = len(df.columns) // nv
-        for i in range(0, nh):
-            for j in range(0, nv):
-                page = df.iloc[(i * rows_per_page):min((i + 1) * rows_per_page, len(df)),
-                       (j * cols_per_page):min((j + 1) * cols_per_page, len(df.columns))]
-                fig = _draw_as_table(page, pagesize)
-                if nh > 1 or nv > 1:
-                    # Add a part/page number at bottom-center of page
-                    fig.text(0.5, 0.5 / pagesize[0],
-                             "Part-{}x{}: Page-{}".format(i + 1, j + 1, i * nv + j + 1),
-                             ha='center', fontsize=8)
-                pdf.savefig(fig, bbox_inches='tight')
+    data = pd.read_csv("snelstartfout.xlsx")
+    table_data = []
+    for i, row in data.iterrows():
+        table_data.append(list(row))
 
-                plt.close()
+    table = Table(table_data)
+    table.setStyle(table_style)
+    pdf_table = []
+    pdf_table.append(df)
 
-
-def create_pdf(df, name):
-    L = len(df.index)
-    npages = int(np.ceil(L / 50))
-    dataframe_to_pdf(df, name, numpages=(npages, 1), pagesize=(8.5, 20))
-
+    pdf.build(pdf_table)
 
 def file_selection(path):
     # returns the selected excel files in the order [website, snelstart, onbekend]
@@ -60,9 +49,7 @@ def file_selection(path):
         csv_files.pop(index)
     return results
 
-
 WEBSITE_MATCH_NAME = "ID-nummer"  # can be any of the column names from the excel file
-
 
 def clean_website(df):
     # Finds the starting position of the table in the excel file such that it is impossible to paste it incorrectly into excel
@@ -81,10 +68,8 @@ def clean_website(df):
     df = df.astype({WEBSITE_MATCH_NAME: 'int'})  # setting id column to integers
     return df
 
-
 SNELSTART_COLUMN_NAME = 'KLANTRelatiecodeNaamPlaats'
 SNELSTART_NEW_COLUMNS = [WEBSITE_MATCH_NAME, 'naam', 'plaats']
-
 
 def format_snelstart(df):
     # seperates the SNELSTART_COLUMN_NAME into SNELSTART_NEW_COLUMNS
@@ -96,11 +81,9 @@ def format_snelstart(df):
     # Reformats the snelstart dataframe
     return df
 
-
 ONBEKEND_NAAM = 'unkown customer name'
 ALLIAS_ID_NUMMER = [WEBSITE_MATCH_NAME, 'ID', 'id', 'nummer', 'studenten', 'student-id', 'student', 'studenten nummer']
 ALLIAS_NAAM = [ONBEKEND_NAAM, 'naam', 'klant', 'name', 'First name']
-
 
 def clean_onbekend(df):
     n_id = 0;
@@ -129,38 +112,46 @@ def clean_onbekend(df):
 
     return df
 
-
 CORRECT_SNELSTART_WEBSITE_COLUMN_NAME = "SNELSTART-WEBSITE_CORRECT"
 CORRRECT_NOT_PAID_NAME = "NOT_PAID_CORRECT"
 AFGEREKEND = 'Afgerekend'  # names of used columns
 OMZETAANTAL = 'OmzetAantal'
 
-
 def correct_website_snelstart(df):
     #
     df[OMZETAANTAL] = df[OMZETAANTAL].fillna(0)  # filling empty cells with zero's to make boolean statement easier
     # finding the correct data and writing data
-    df[CORRECT_SNELSTART_WEBSITE_COLUMN_NAME] = np.where((df[OMZETAANTAL] == 1) & (df[AFGEREKEND] == 'Ja'), True,
-                                                         False)  # finding correct inboekingen
-    df[CORRRECT_NOT_PAID_NAME] = np.where((df[OMZETAANTAL] == 0) & (df[AFGEREKEND] == 'Nee'), True,
-                                          False)  # finding correct not paid
+    df[CORRECT_SNELSTART_WEBSITE_COLUMN_NAME] = np.where((df[OMZETAANTAL] == 1) & (df[AFGEREKEND] == 'Ja'), 1,
+                                                         0)  # finding correct inboekingen
+    df[CORRRECT_NOT_PAID_NAME] = np.where((df[OMZETAANTAL] == 0) & (df[AFGEREKEND] == 'Nee'), 1,
+                                          0)  # finding correct not paid
     return df
-
 
 CORRECT_SNELSTART_ONBEKEND = 'ONBEKEND-SNELSTART_MATCH'  #
 CORRECT_WEBSITE_ONBEKEND = 'ONBEKEND-WEBSITE_MATCH'
 
-
 def correct_onbekend(df):
-    df[CORRECT_SNELSTART_ONBEKEND] = np.where((df[OMZETAANTAL] == 1) & (df[ONBEKEND_NAAM].notnull()), True, False)
-    df[CORRECT_WEBSITE_ONBEKEND] = np.where((df[AFGEREKEND] == 'Ja') & (df[ONBEKEND_NAAM].notnull()), True, False)
+    df[CORRECT_SNELSTART_ONBEKEND] = np.where((df[OMZETAANTAL] == 1) & (df[ONBEKEND_NAAM].notnull()), 1, 0)
+    df[CORRECT_WEBSITE_ONBEKEND] = np.where((df[AFGEREKEND] == 'Ja') & (df[ONBEKEND_NAAM].notnull()), 1, 0)
     return df
 
+def correct_result(df):
+    # Participant check functionality:
+    # Check for GOOD cases specifacally and assign a true or false accordingly
+    #      -> importantly these are very specific and simple correct cases
+    # If done for all the the true cases, only one case should be true for each inboeking
+    #      -> If all are false something is incorrect
+    #      -> If more than 2 are true something is incorrect -> 2 cases can't be correct at the same time
+    df['sum'] = df[CORRECT_SNELSTART_WEBSITE_COLUMN_NAME] + df[CORRRECT_NOT_PAID_NAME] + df[CORRECT_WEBSITE_ONBEKEND]
+    df['result'] = df['sum'] == 1
+
+
+    return df
 
 if __name__ == '__main__':
     os_path = (os.getcwd())
     test_path = os_path + "\\testdata"
-    # filepaths = file_selection(test_path) #selecting the files to be website, snelstart, unkown
+    #  filepaths = file_selection(test_path) #selecting the files to be website, snelstart, unkown
 
     # TEST
     # filepaths = [test_path + '\\web.xlsx', test_path + '\\asml.xlsx', test_path + '\\onbekend.xlsx']  #loading test files
@@ -181,27 +172,9 @@ if __name__ == '__main__':
     # todo onbekende klant data invoeg functie toevoegen
     merged.to_excel("RAW DATA.xlsx")
 
-    ## Participant check functionality:
-    ## Check for GOOD cases specifacally and assign a true or false accordingly
-    ##      -> importantly these are very specific and simple correct cases
-    ## If done for all the the true cases, only one case should be true for each inboeking
-    ##      -> If all are false something is incorrect
-    ##      -> If more than 2 are true something is incorrect -> 2 cases can't be correct at the same time
-    snelstart_website_correct = correct_website_snelstart(
-        merged)  # checking between website and snelstart correct values
 
-    df = correct_onbekend(snelstart_website_correct)
-    ##TODO dit filter verbeteren want het ziet er ruk uit
-    df['result'] = np.where((
-                                    (df[CORRECT_SNELSTART_WEBSITE_COLUMN_NAME] == True) & (df[
-                                CORRRECT_NOT_PAID_NAME] == False) & (df[CORRECT_WEBSITE_ONBEKEND] == False) |
-                                    (df[CORRECT_SNELSTART_WEBSITE_COLUMN_NAME] == False) & (df[
-                                        CORRRECT_NOT_PAID_NAME] == True) & (df[CORRECT_WEBSITE_ONBEKEND] == False) |
-                                     (df[CORRECT_SNELSTART_WEBSITE_COLUMN_NAME] == False) & (df[
-                                        CORRRECT_NOT_PAID_NAME] == False) & (df[CORRECT_WEBSITE_ONBEKEND] == True))
-                            & (df[CORRECT_SNELSTART_ONBEKEND] == False), True, False)
-    # create_pdf(snelstart_website_correct, 'result.pdf') #writing out final results to pdf
-    df.to_excel('snelstartfout.xlsx')  # writing out resulting data
-    # todo onbekende klant controle toevoegen
-    #   * onbekend - website match
-    #   * onbekend - snelstart match
+    snelstart_website_correct = correct_website_snelstart(merged)  # checking between website and snelstart correct values
+    df = correct_onbekend(snelstart_website_correct) #checking matches between onbekend and snelstart + website
+    df = correct_result(df) #doing the final check if all results are correct
+    create_pdf(df, 'result.pdf') #writing out final results to pdf
+    df.to_excel('snelstartfout.xlsx')  # writing out resulting data to excel
